97fee83e0d40a6e5d6d8817beedc7465d933aedb
spiritking416 <120049125@qq.com>
2017-02-24 17:43:25 +0800

    201702241743

--- INDNC/INDNC/UserControlMachineState.cs
+++ INDNC/INDNC/UserControlMachineState.cs
@@ -20,10 +20,9 @@ namespace INDNC
         public void ListViewDraw()
         {
             this.listView1.Columns.Add("机床号",274, HorizontalAlignment.Left);
-
             this.listView1.Columns.Add("机床状态", 274, HorizontalAlignment.Left);
-
             this.listView1.Columns.Add("当前告警", 274, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("发生时间", 274, HorizontalAlignment.Left);
             //this.listView1.Columns.Add("列标题1", 120, HorizontalAlignment.Left); //一步添加  
             /*this.listView1.BeginUpdate();   //数据更新，UI暂时挂起，直到EndUpdate绘制控件，可以有效避免闪烁并大大提高加载速度  
             
@@ -46,5 +45,6 @@ namespace INDNC
 
 
         }
+
     }
 }
