2a0316e0de983bd54eb0cda6bf4e315fbb90bd3c
spiritking416 <120049125@qq.com>
2017-04-07 15:15:37 +0800

    201704071515

--- INDNC/INDNC/UserControlAlarmAnlysis.cs
+++ INDNC/INDNC/UserControlAlarmAnlysis.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Data;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms.DataVisualization.Charting;
 using ServiceStack.Redis;
+
+using System.Text;
+
 
 namespace INDNC
 {
@@ -15,6 +19,7 @@ namespace INDNC
         List<MachineInfo> CNCinfo = new List<MachineInfo>();
         List<MachineInfo> Robotinfo = new List<MachineInfo>();
         RedisPara serverpara;  //服务器参数
+        RedisManager redismanager = new RedisManager();
         public List<MachineInfo> CNCinfoName
         {
             set
@@ -41,22 +46,23 @@ namespace INDNC
         public UserControlAlarmAnlysis()
         {
             InitialDraw();
+            myTab.Columns.Add("ID", Type.GetType("System.Int32"));
+            myTab.Columns.Add("Yvalue", Type.GetType("System.Int64"));
+            myTab.Columns.Add("Xvalue", Type.GetType("System.String"));
+            myTab.Columns[0].AutoIncrement = true;
+            myTab.Columns[0].Unique = true;
+            myTab.Columns[0].AutoIncrementSeed = 1;
+            myTab.Columns[0].AutoIncrementStep = 1;
+            chart1.Series[0].YValueMembers = "Yvalue";
+            chart1.Series[0].XValueMember = "Xvalue";
+            chart1.Series[0].LegendToolTip = "机床的历史告警数目"; // 鼠标放到系列上出现的文字  
+            chart1.Series[0].LegendText = "历史告警"; // 系列名字  
+            chart1.Series[0].XValueType = ChartValueType.Date;
         }
 
         private void InitialDraw()
         {
             InitializeComponent();
-
-            chart1.Series[0].YValueMembers = "Yvalue";
-            chart1.Series[0].XValueMember = "Xvalue";
-            chart1.Series[0].LegendToolTip = "我的成绩"; // 鼠标放到系列上出现的文字  
-            chart1.Series[0].LegendText = "当前告警"; // 系列名字  
-            chart1.Series[0].XValueType = ChartValueType.Date;
-
-            InitialTab();
-
-            chart1.DataSource = myTab;
-            chart1.DataBind();
         }
 
         private void InitialTab()
@@ -66,32 +72,47 @@ namespace INDNC
             {
                 //从连接池获得只读连接客户端
                 int initialDB = 0;
-                RedisClient Client = new RedisClient();
+                RedisClient Client = (RedisClient)redismanager.GetReadOnlyClient(ref (initialDB), ref (host));
                 if (Client == null || !Client.Ping())
                 {
                     throw new Exception("连接服务器失败，请设置服务器参数!");
                 }
                 //连接云端服务器成功
+
+                myTab.Clear();
+                foreach (var key in CNCinfo)
+                {
+                    Client.ChangeDb(key.MachineDB);
+
+                    byte[] machinealarmbyte = new byte[] { };
+                    byte[] alarmbyte = Encoding.UTF8.GetBytes("ALARMNUM_HISTORY");
+                    machinealarmbyte = Client.HGet("Alarm:AlarmNum", alarmbyte);
+                    if (machinealarmbyte == null)
+                        continue;
+                    string machinealarmstr = System.Text.Encoding.Default.GetString(machinealarmbyte);
+                    long machinealarm = Convert.ToInt64(machinealarmstr);
+
+                    myTab.Rows.Add(new object[] { null, machinealarm, key.MachineName });
+                }
+                foreach (var key in Robotinfo)
+                {
+                    Client.ChangeDb(key.MachineDB);
+
+                    byte[] machinealarmbyte = new byte[] { };
+                    byte[] alarmbyte = Encoding.UTF8.GetBytes("ALARMNUM_HISTORY");
+                    machinealarmbyte = Client.HGet("Alarm:AlarmNum", alarmbyte);
+                    if (machinealarmbyte == null)
+                        continue;
+                    string machinealarmstr = System.Text.Encoding.Default.GetString(machinealarmbyte);
+                    long machinealarm = Convert.ToInt64(machinealarmstr);
+
+                    myTab.Rows.Add(new object[] { null, machinealarm, key.MachineName });
+                }
             }
-            catch
+            catch(Exception ex)
             {
-
+                MessageBox.Show("ERROR:" + ex.Message, "ERROR");
             }
-
-                myTab.Columns.Add("ID", Type.GetType("System.Int32"));
-            myTab.Columns[0].AutoIncrement = true;
-            myTab.Columns[0].Unique = true;
-            myTab.Columns[0].AutoIncrementSeed = 1;
-            myTab.Columns[0].AutoIncrementStep = 1;
-            myTab.Columns.Add("Yvalue", Type.GetType("System.Int32"));
-            myTab.Columns.Add("Xvalue", Type.GetType("System.DateTime"));
-
-            myTab.Rows.Add(new object[] { null, 22D, "2015-02-01 8:00" });
-            myTab.Rows.Add(new object[] { null, 23D, "2015-03-01 9:00" });
-            myTab.Rows.Add(new object[] { null, 25D, "2015-04-01 10:00" });
-            myTab.Rows.Add(new object[] { null, 24D, "2015-05-01 11:00" });
-            myTab.Rows.Add(new object[] { null, 22D, "2015-06-01 12:00" });
-            myTab.Rows.Add(new object[] { null, 21D, "2015-07-01 13:00" });
         }
 
         //绘制
@@ -100,7 +121,8 @@ namespace INDNC
             try
             {
                 InitialTab();
-                chart1.Update();
+                chart1.DataSource = myTab;
+                chart1.DataBind();
             }
             catch
             {
